97b18335b8b02e686a530a44c284691db97e1462
= <=>
2023-05-14 22:36:19 +0700

    version 1

---
+++ babyshop/src/main/java/com/babyshop/babyshop/BabyshopApplication.java
@@ -0,0 +1,13 @@
+package com.babyshop.babyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyshopApplication.class, args);
+	}
+
+}
---
+++ babyshop/src/test/java/com/babyshop/babyshop/BabyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyshop.babyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
